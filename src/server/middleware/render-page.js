6e37fb3ed5cf62fd7bdc8045d2a72b8082a50c75
ValeraS <ValeraS@users.noreply.github.com>
2019-10-12 16:47:02 +0300

    fix: use renderToSring

--- src/server/middleware/render-page.js
+++ src/server/middleware/render-page.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import { renderToStaticMarkup } from 'react-dom/server';
+import { renderToString } from 'react-dom/server';
 import { App } from 'components/App/App@server';
 
 export function renderPage() {
   return (req, res) => {
-    let content = renderToStaticMarkup(
+    let content = renderToString(
       <App state={res.locals.state} url={req.url} />
     );
 
